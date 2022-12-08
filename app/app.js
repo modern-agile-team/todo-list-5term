95737d18b2e9851e7d13652670aa49b0f713f3ef
HyeonJun <beeminor60@gmail.com>
2022-12-08 22:27:49 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const home = require("./src/routes/home");
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use("/",home);
+ 
+ 
+module.exports = app;
---
+++ app/src/routes/home/home.ctrl.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const output = {
+    home: (req, res) => {
+      res.render("home/index");
+    },
+};
+
+module.exports = {
+    output,
+}
