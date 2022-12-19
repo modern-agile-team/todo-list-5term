2b3eb550e033bfeb8b6a438c1ecfad91ca1cf3ba
현상진 <hyunsangjin@hyeonsangjin-ui-MacBook-Pro.local>
2022-12-19 17:39:19 +0900

    기능구현까지는 완료

--- main.js
+++ main.js
@@ -1,57 +1,94 @@
-'use strict';
+"use strict";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const input = document.querySelector('#todo')
-  const addButton = document.querySelector('#add-button')
-  const todoList = document.querySelector('#todo-list')
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.querySelector("#todo");
+  const addButton = document.querySelector("#add-button");
+  const todoList = document.querySelector("#todo-list");
 
-  addButton.addEventListener('click', addTodo)
+  addButton.addEventListener("click", addTodo);
 
-    function addTodo() {
-    console.log(input)
-    if (input.value === '') return alert('입력하세요')
-      const item = document.createElement('div')
+  let itemID = 0;
 
-      const checkbox = document.createElement('input')
-      checkbox.type = 'checkbox'
+  function addTodo() {
+    itemID++;
 
-      const text = document.createElement('span');
+    console.log(input);
+    if (input.value === "") return alert("입력하세요");
+    const item = document.createElement("div");
+    item.setAttribute("id", itemID);
 
-      const deleteButton = document.createElement('button')
-      deleteButton.textContent = "제거하기"
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
 
-      text.textContent = input.value
-      input.value = ''
+    const updateText = document.createElement("input");
+    updateText.setAttribute("class", "가리기");
+    updateText.type = "text";
+    updateText.value = input.value
 
-      item.appendChild(checkbox)
-      item.appendChild(text)
-      item.appendChild(deleteButton)
-      todoList.appendChild(item)
-
-
-      checkbox.addEventListener('change', (event) => {
-        if (event.currentTarget.checked) {
-          text.style.textDecoration = 'line-through'
-        }
-        else {
-          text.style.textDecoration = 'none'
-        }
-      })
+    const updateFin = document.createElement("button")
+    updateFin.textContent = "완료"
+    updateFin.setAttribute("class","가리기");
+    updateText.type = "";
+    updateText.value = input.value
 
 
-      deleteButton.addEventListener('click', (event) => {
-        todoList.removeChild(event.currentTarget.parentNode)
-      })
-      input.value = ''
-      // alert.textContent = ''
-    
+    const text = document.createElement("span");
+
+    const editButton = document.createElement("button");
+    editButton.textContent = "수정하기";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "제거하기";
+
+    text.textContent = input.value;
+    input.value = "";
+
+    item.appendChild(checkbox);
+    item.appendChild(text);
+    item.appendChild(updateText);
+    item.appendChild(editButton);
+    item.appendChild(updateFin);
+    item.appendChild(deleteButton);
+    todoList.appendChild(item);
+
+    checkbox.addEventListener("change", (event) => {
+      if (event.currentTarget.checked) {
+        text.style.textDecoration = "line-through";
+      } else {
+        text.style.textDecoration = "none";
+      }
+    });
+
+    editButton.addEventListener("click", (event) => {
+      updateText.setAttribute("class", "");
+      text.setAttribute("class", "가리기");
+      updateFin.setAttribute("class", "");
+      editButton.setAttribute("class", "가리기")
+    });
+
+    updateFin.addEventListener("click", (event) => {
+      updateText.setAttribute("class", "가리기");
+      text.setAttribute("class", "");
+      updateFin.setAttribute("class", "가리기");
+      editButton.setAttribute("class", "")
+      console.log(updateText.value)
+      text.textContent = updateText.value
+    });
+
+
+    deleteButton.addEventListener("click", (event) => {
+      todoList.removeChild(event.currentTarget.parentNode);
+    });
+    input.value = "";
+    // alert.textContent = ''
+  }
+  function update(edit) {
+    console.log(edit);
   }
 
-
-
-  input.addEventListener('keypress', (event) => {
-    const ENTER = 13
-    if (event.keyCode === ENTER)
-      addTodo();
-  })
-})
+  input.addEventListener("keypress", (event) => {
+    console.log(event)
+    const ENTER = 13;
+    if (event.keyCode === ENTER) addTodo();
+  });
+});
